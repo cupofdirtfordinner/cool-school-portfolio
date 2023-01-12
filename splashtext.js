fa5b7c762b124016b36f696f2bbfc4d48eecf9f5
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2023-01-12 12:29:35 -0600

    Add files via upload

---
+++ splashtext.js
@@ -0,0 +1,11 @@
+setInterval(10)
+fetch("lines.json")
+  .then(res => res.json())
+  .then(json => {
+    const splash = document.getElementById("splash")
+    splash.innerHTML = json[Math.floor(Math.random() * json.length)];
+    splash.style.animationDuration = `${Math.round(splash.clientWidth / 200)}s`
+  }).catch(error => {
+    console.log(error);
+  });
+
